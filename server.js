0ecc5f2e551a5fcea1650b30ff50511f323df1a0
pharaohlxvi <pharaohlxvi@gmail.com>
2018-06-10 21:57:14 -0300

    server.js

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback')
 // ^ middleware to redirect all URLs to index.html
 
-let app = express();
-const staticFileMiddleware = express.static(path.join(__dirname))
-app.use(serveStatic(__dirname + "/dist"));
-app.use(staticFileMiddleware)
+var app = express();
 app.use(history())
-app.use(staticFileMiddleware)
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/index.html'))
-})
+app.set(port, (process.env.PORT || 8080)); //5000
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port)
+app.listen(app.get(port), () => {
+// console.log(ENV: ${process.env.NODE_ENV});
+// console.log(Derp is running at localhost: ${app.get('port')});
 });
